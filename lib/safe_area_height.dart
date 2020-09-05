ab08978a9f4ab09438f6fed09f6023f5d222d56c
Max Mason <Max8648@hotmail.com>
2020-09-05 22:45:20 +0100

    fix2

--- lib/safe_area_height.dart
+++ lib/safe_area_height.dart
@@ -3,16 +3,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class SafeAreaHeight {
-  
-
   static Future<double> get safeAreaHeightTop async {
-    static const MethodChannel _channel = const MethodChannel('safe_area_height');
+    const MethodChannel _channel = const MethodChannel('safe_area_height');
     final double height = await _channel.invokeMethod('getSafeAreaHeightTop');
     return height;
   }
 
   static Future<double> get safeAreaHeightBottom async {
-    static const MethodChannel _channel = const MethodChannel('safe_area_height');
+    const MethodChannel _channel = const MethodChannel('safe_area_height');
     final double height = await _channel.invokeMethod('getSafeAreaHeightBottom');
     return height;
   }
